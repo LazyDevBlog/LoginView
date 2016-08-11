311f4462714a25e07d9b6108380297377341e18c
TBLStudio <iosdev.tbl@gmail.com>
2016-08-11 11:13:44 +0700

    Add resource, Init Viewcontroller

---
+++ LoginViewDemo/LoginViewDemo/ViewControllers/MainVC.swift
@@ -0,0 +1,36 @@
+//
+//  MainVC.swift
+//  LoginViewDemo
+//
+//  Created by Thái Bô Lão on 8/11/16.
+//  Copyright © 2016 TBLStudio. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
